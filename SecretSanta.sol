845f3a347d54e6f991d45e0303d6d7f20f6851c6
Brandonisernest <48809099+Brandonisernest@users.noreply.github.com>
2021-11-22 17:49:21 -0500

    Add in ranking list to assign gift recipients

--- SecretSanta.sol
+++ SecretSanta.sol
@@ -5,7 +5,7 @@ General walkthrough
     A) for this version, make it a humble secret santa...not everybody is rich!
     B) 10 eth limit
 2. The floor price needs $20 USD (0.004803 Ether as of 11/30) - implement oracle? - Chainlink for Eth price?
-    A) https://www.google.co
+    A) https://data.chain.link/ethereum/mainnet/crypto-usd/eth-usd
 3. Important! When you enter a santa struct, ALL you need to enter and specify are the gift name, value of the gift at time of struct creation, link to the gift (limit to amazon) 
     A) Right now, I want to not actually send the "gift" but the gift value!
     B) The information provided (i.e. gift name....it creates a gift Struct)
@@ -27,6 +27,13 @@ General walkthrough
     A) mapping(address => giftStruct);
     
     
+New Flow:
+1. Enter into contract by creating gift
+2. Others also enter contract by creating gift
+3. The previous entrant will give gift to new entrant
+4. Assign the new entrant the ownership of the gift struct
+5. When the timer expires, previous entrant sends eth value to new entrant
+6. the new entrant get's access (visually on webpage) to the gift and has the option to purchase with newly received eth!
     
 To Dos:
 1. Create events interface 
@@ -46,9 +53,16 @@ contract SecretSanta {
     mapping(address => giftStruct) public giftOriginatorMapping;
     //who owns the gift Struct
     mapping(address => giftStruct) giftOwnershipMapping;
-
+    //gift giving mapping
+    mapping(address => address) giftDestinationMapping;
+    //mapping used to order entrants
+    mapping(address => uint) rankMapping;
     //arrays
     address[] public groupParticipantsArray;
+    //Guard
+    address constant GUARD = address(1);
+    //arbitrary counter for giftDestinationMapping
+    uint arbitraryCounter = 0;
     
     struct giftStruct{
         string giftName;
@@ -60,6 +74,7 @@ contract SecretSanta {
         santa = msg.sender;
         //arbitrary endTime
         endTime = block.timestamp * 2;
+        giftDestinationMapping[GUARD] = GUARD;
     }
     
     modifier onlySanta() {
@@ -95,13 +110,27 @@ contract SecretSanta {
         
         //add to giftOriginatorMapping;
         giftOriginatorMapping[msg.sender] = newGiftStruct;
+        //push new entrant into groupParticipantsArray
+        groupParticipantsArray.push(msg.sender);
         
+        //update giftDestinationMapping via linkedList approach
+        
+        //index for rankMapping
+        address index = _findIndex(arbitraryCounter);
+        //update rank table
+        giftDestinationMapping[msg.sender] = giftDestinationMapping[index];
+        giftDestinationMapping[index] = msg.sender;
+        
+        //increment counter
+        arbitraryCounter++;
         
     }
     
     //santa distributes gift one endTime has passed
     function distributeGifts() public payable onlySanta{
         require(block.timestamp > endTime);
+        
+        //All I need to do is reassign gift ownership
         
         
     }
@@ -111,5 +140,32 @@ contract SecretSanta {
         return groupParticipantsArray;
         
     }
+    
+    
+    function _verifyIndex(address prevAddress, uint256 newValue, address nextAddress) 
+        internal 
+        view 
+        returns(bool) {
+            return (prevAddress == GUARD || rankMapping[prevAddress] >= newValue) && 
+                   (nextAddress == GUARD || newValue > rankMapping[nextAddress]);
+
+    }
+
+
+    //returns the index of the value, which is an address
+    function _findIndex(uint256 newValue) 
+        internal 
+        view 
+        returns(address) {
+            address candidateAddress = GUARD;
+
+            while(true){
+                if(_verifyIndex(candidateAddress, newValue, giftDestinationMapping[candidateAddress])){
+                    return candidateAddress;
+                }
+                candidateAddress = giftDestinationMapping[candidateAddress];
+            }
+
+        }
     
 }
