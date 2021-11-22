6d05d60da4bffabb1f63f42613c4969ccebbe8e6
Brandonisernest <48809099+Brandonisernest@users.noreply.github.com>
2021-11-22 14:42:47 -0500

    Update walkthrough and added code

--- SecretSanta.sol
+++ SecretSanta.sol
@@ -12,15 +12,54 @@ General walkthrough
 5. After minimum number of address have entered santa Struct (5), prevent further entry into that struct
 6. Within the santa Struct, include:
     A) Gift Struct
-    B) Mapping with price range to Gift Struct
-    C) Mapping with address to Gift Struct
+    B) Group Struct
+    C) Mapping with price range to Gift Struct
+    D) Mapping with address to Gift Struct
         a) Only allow one address to enter one gift struct for now
-7. Within each gift Struct:
+7. Within each group Struct:
     C) Mapping of Address => "string" home address? (optional)
     B) Uint counter for 
-7. After a certain time (block height)has passed and not enough people entered a santa struct, close out the struct and allow the addresses to enter into a new struct with new price
-8. Pseudorandomly assign gift to each person
+8. Gift struct:
+    A) Name of Gift
+    B) Value of Gift
+    C) Link of Gift
+9. After a certain time (block height)has passed and not enough people entered a santa struct, close out the struct and allow the addresses to enter into a new struct with new price
+10. Pseudorandomly assign gift to each person
     A) Make sure the person can't give gift to themselves
-9. Allow person to choose to either receive the gift or just keep the cash value
+11. Allow person to choose to either receive the gift or just keep the cash value
+12. You transfer ownership of giftStruct via Mapping
+    A) mapping(address => giftStruct);
 
 */
+
+pragma solidity 0.8.6;
+
+contract SecretSanta {
+    //owner of the contract. In case for permissioning
+    address public santa;
+    mapping(address => giftStruct) public giftOwnershipMapping;
+    mapping(uint => giftStruct) public giftValueMapping;
+    
+    struct groupStruct {
+        giftStruct giftStruct;
+        uint participantCounter;
+        //mailing address mapping optional for now
+        // mapping(address => string) mailingAddressMapping;
+        
+    }
+    
+    struct giftStruct{
+        string giftName;
+        uint giftValue;
+        string giftUrl;
+    }
+    
+    constructor() public {
+        santa = msg.sender;
+    }
+    
+    modifier onlySanta() {
+        require(msg.sender == santa, 'Only Santa can do this!');
+        _;
+    }
+}
