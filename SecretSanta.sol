b676f3f52ba51ec6e654a127e4d573d5de80921a
Brandonisernest <48809099+Brandonisernest@users.noreply.github.com>
2021-11-22 15:13:04 -0500

    Added enter and group logic

--- SecretSanta.sol
+++ SecretSanta.sol
@@ -9,7 +9,7 @@ General walkthrough
     A) Right now, I want to not actually send the "gift" but the gift value!
     B) The information provided (i.e. gift name....it creates a gift Struct)
     C) price value will put you in mapping
-5. After minimum number of address have entered santa Struct (5), prevent further entry into that struct
+5. No limit to entrants to a group. 
 6. Within the santa Struct, include:
     A) Gift Struct
     B) Group Struct
@@ -23,12 +23,14 @@ General walkthrough
     A) Name of Gift
     B) Value of Gift
     C) Link of Gift
-9. After a certain time (block height)has passed and not enough people entered a santa struct, close out the struct and allow the addresses to enter into a new struct with new price
+9. After a certain time (block height)has passed and only one person is in a group struct, close out the struct and allow the addresses to enter into a new struct with new price
 10. Pseudorandomly assign gift to each person
     A) Make sure the person can't give gift to themselves
+    B) Distribute gift once timer ends
 11. Allow person to choose to either receive the gift or just keep the cash value
 12. You transfer ownership of giftStruct via Mapping
     A) mapping(address => giftStruct);
+    
 
 */
 
@@ -37,10 +39,15 @@ pragma solidity 0.8.6;
 contract SecretSanta {
     //owner of the contract. In case for permissioning
     address public santa;
+    //who created the gift struct?
+    mapping(address => giftStruct) public giftOriginatorMapping;
+    //who owns the gift Struct
     mapping(address => giftStruct) public giftOwnershipMapping;
-    mapping(uint => giftStruct) public giftValueMapping;
+    //what is the value of the groupStruct
+    mapping(uint => groupStruct) public giftValueMapping;
     
     struct groupStruct {
+        uint giftValueRange;
         giftStruct giftStruct;
         uint participantCounter;
         //mailing address mapping optional for now
@@ -56,10 +63,59 @@ contract SecretSanta {
     
     constructor() public {
         santa = msg.sender;
+        //anything else?
+        
     }
     
     modifier onlySanta() {
         require(msg.sender == santa, 'Only Santa can do this!');
         _;
     }
+    
+    
+    function groupStructHandler(giftStruct memory _giftStruct) public {
+        
+        uint newGiftValue = _giftStruct.giftValue;
+        //alter in the future to add in oracle data
+        //initial giftValues are ridiculous for testing purposes
+        if(newGiftValue >= 1 ether && newGiftValue< 5 ether) {
+             groupStruct memory newGroupStruct = groupStruct({
+                 giftValueRange: 1,
+                 giftStruct :_giftStruct,
+                 //work on the counter later
+                 participantCounter: 0
+             });
+             
+             giftValueMapping[1] = newGroupStruct;
+        }
+        else if (newGiftValue >= 5 ether && newGiftValue < 10 ether) {
+             groupStruct memory newGroupStruct = groupStruct({
+                 giftValueRange: 2,
+                 giftStruct :_giftStruct,
+                 //work on the counter later
+                 participantCounter: 0
+             });
+             
+             giftValueMapping[2] = newGroupStruct;
+        }
+        
+        
+    }
+    
+    function enterSecretsanta(string memory _giftName, uint _giftValue, string memory _giftUrl) public {
+        
+        giftStruct memory newGiftStruct = giftStruct({
+            giftName : _giftName,
+            giftValue : _giftValue,
+            giftUrl : _giftUrl
+        });
+        
+        //add to giftOriginatorMapping;
+        giftOriginatorMapping[msg.sender] = newGiftStruct;
+        
+        //add to group
+        groupStructHandler(newGiftStruct);
+        
+    }
+    
 }
